d308d321f1018a42b1a69aedd166c3b6e334b965
Andreas Ahlenstorf <andreas@ahlenstorf.ch>
2026-02-13 19:56:46 +0000

    test: simplify test of service health checks (#1370)
    The test is overly complicated and relies on putting MySQL in an unhealthy state instead of making the health check fail. Furthermore, it takes long for no particular reason.
    
    <!--start release-notes-assistant-->
    <!--URL:https://code.forgejo.org/forgejo/runner-->
    - other
      - [PR](https://code.forgejo.org/forgejo/runner/pulls/1370): <!--number 1370 --><!--line 0 --><!--description dGVzdDogc2ltcGxpZnkgdGVzdCBvZiBzZXJ2aWNlIGhlYWx0aCBjaGVja3M=-->test: simplify test of service health checks<!--description-->
    <!--end release-notes-assistant-->
    
    Reviewed-on: https://code.forgejo.org/forgejo/runner/pulls/1370
    Reviewed-by: Mathieu Fenniak <mfenniak@noreply.code.forgejo.org>
    Co-authored-by: Andreas Ahlenstorf <andreas@ahlenstorf.ch>
    Co-committed-by: Andreas Ahlenstorf <andreas@ahlenstorf.ch>

--- act/runner/runner_test.go
+++ act/runner/runner_test.go
@@ -354,7 +354,7 @@ func TestRunner_RunEvent(t *testing.T) {
 		{workdir, "services", "push", "", platforms, secrets},
 		{workdir, "services-with-container", "push", "", platforms, secrets},
 		{workdir, "mysql-service-container-with-health-check", "push", "", platforms, secrets},
-		{workdir, "mysql-service-container-premature-terminate", "push", "service [maindb]", platforms, secrets},
+		{workdir, "service-container-premature-terminate", "push", "service [nginx]", platforms, secrets},
 		{workdir, "services-empty-image", "push", "", platforms, secrets},
 		{workdir, "services-options-group-add", "push", "", platforms, secrets},
 		{workdir, "services-options-user", "push", "", platforms, secrets},
